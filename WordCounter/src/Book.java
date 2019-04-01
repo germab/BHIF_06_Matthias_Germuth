db87047b7f2f9d42510759effc510c7d86b412c9
germab <germab16@new.edvhtl.at>
2019-04-01 11:01:14 +0200

    Producer and Book Class

--- WordCounter/src/Book.java
+++ WordCounter/src/Book.java
@@ -1,3 +1,6 @@
+
+import java.util.HashMap;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,15 @@
  * @author Matthias
  */
 public class Book {
+    private String fileName;
+    private String text;
+
+    public Book(String fileName, String text) {
+        this.fileName = fileName;
+        this.text = text;
+    }
     
+    public HashMap<String,Integer> countWords(){
+        
+    }
 }
